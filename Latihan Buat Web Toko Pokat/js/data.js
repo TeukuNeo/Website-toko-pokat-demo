819dcb4a3c128eb1a9e644a6760ac540849f0cac
TeukuNeo <155743925+TeukuNeo@users.noreply.github.com>
2025-01-11 17:06:00 +0700

    Add files via upload

---
+++ Latihan Buat Web Toko Pokat/js/data.js
@@ -0,0 +1,22 @@
+
+let productGenerate = [{
+    id: "BL",
+    name: "Avocado Ice",
+    price: 5000,
+    img: "../img/avocado-ice-cream.jpg"
+},{
+    id: "SG",
+    name: "Avocado Coffee",
+    price: 6500,
+    img: "../img/avocado-coffee.jpg"
+},{
+    id: "CD",
+    name: "Avocado Juice",
+    price: 6000,
+    img: "../img/product.jpeg"
+},{
+    id: "FM",
+    name: "Avocado Latte",
+    price: 7500,
+    img: "../img/avocado-latte.webp"
+}]
